b45c20381f96e0048cbdcc7ca70c73274a3b7181
Warren <warrenpaxon@gmail.com>
2025-06-08 10:18:05 -0500

    update loading time

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-4 text-blue-900 dark:text-white">
       {/* <img src={profileImage} alt="Brandon Tanaka" className="w-32 h-32 rounded-full mb-6 shadow-lg" /> */}
-      <h1 className="text-3xl font-Bungee font-bold mb-2">Brandon Tanaka - 1</h1>
+      <h1 className="text-3xl font-Bungee font-bold mb-2">Brandon Tanaka</h1>
       <p className="text-lg font-balsamiq max-w-2xl text-justify">
         I’m a Software Engineer with a strong background in software development, focused on building reliable and user-friendly systems.
         I enjoy taking complex challenges and turning them into simple, effective solutions that work well in the real world.
